cbe82430b9d3d8ceaa6b9b533829a7b79a98a49d
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2021-12-23 19:16:24 -0300

    Cria classe Proprietário

---
+++ model/Proprietor.js
@@ -0,0 +1,47 @@
+import {Storage } from './Storage.js';
+
+class Proprietor {
+    constructor(name, contact) {
+        this.name = name;
+        this.contact = contact;
+    }
+
+    // ----- CRUD -----
+
+    static create(proprietor) {
+        const proprietors = Storage.getProprietors();
+
+        proprietors.push(proprietor);
+        Storage.setProprietors(proprietors);
+    };
+
+    static read() {
+        return Storage.getProprietors();
+    };
+
+    static update(updatedProprietor, receivedId) {
+        const proprietors = Storage.getProprietors();
+
+        proprietors.forEach((proprietor, index) => {
+            if(proprietor.id === receivedId) {
+                proprietors[index] = updatedProprietor;
+            }
+        });
+
+        Storage.setProprietors(proprietors);
+    };
+
+    static delete(receivedId) {
+        const proprietors = Storage.getProprietors();
+
+        proprietors.forEach((proprietor, index) => {
+            if(proprietor.id === receivedId) {
+                proprietors.splice(index, 1);
+            }
+        });
+
+        Storage.setProprietors(proprietors);
+    };
+};
+
+export { Proprietor }

1a83e33faeafa8abae21faa22cde08616954dd8c
Israel Amaral <64231004+imdoamaral@users.noreply.github.com>
2021-12-23 23:46:27 -0300

    Cria evento de remover e editar

--- mainProprietor.js
+++ mainProprietor.js
@@ -1,5 +1,7 @@
 import { UI } from './controller/ProprietorUIController.js';
 import { Form } from './controller/ProprietorFormController.js';
+import { Proprietor } from './model/Proprietor.js';
+import { Modal } from './controller/ProprietorModalController.js';
 
 // Evento: Listar proprietários
 document.addEventListener('DOMContentLoaded', UI.showProprietors());
@@ -10,3 +12,26 @@ document.querySelector('#proprietor-form').addEventListener('submit', (event) =>
 });
 
 // Evento: Remover/Editar um proprietário
+document.querySelector('#proprietor-list').addEventListener('click', (event) => {
+    // remover
+    if(event.target.classList.contains('delete')) {
+        const id = event.target.parentElement.parentElement.firstElementChild.textContent;
+
+        Proprietor.delete(id);
+        UI.removeProprietor(event.target);
+        UI.showAlert('Proprietário deletado', 'success');
+    }
+
+    // editar
+    if(event.target.classList.contains('edit')) {
+        const id = parseInt(event.target.parentElement.parentElement.firstElementChild.textContent);
+
+        Modal.open();
+        Modal.fillFields(id);
+
+        document.querySelector('#modal-form').addEventListener('submit', (event) => {
+            Modal.submit(event, id);
+            Modal.close();
+        })
+    }
+})
